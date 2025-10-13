9bb703dba6212d905949b772004ebbb818929e9d
Bandi Rajashree <bandirs2003@gmail.com>
2025-10-13 21:08:43 +0530

    (feat) : add basic UI

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+import { Routes,Route} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import HomePage from './pages/HomePage'
+import SignUpPage from './pages/SignUpPage'
+import LoginPage from './pages/LoginPage'
+import SettingsPage from './pages/SettingsPage'
+import ProfilePage from './pages/ProfilePage'
+
+function App() {
+
+  return (
+      <div>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element ={<HomePage/>}></Route>
+          <Route path="/signup" element={<SignUpPage/>}></Route>
+           <Route path="/login" element={<LoginPage/>}></Route>
+           <Route path="/settings" element={<SettingsPage/>}></Route>
+           <Route path="/profile" element={<ProfilePage/>}></Route>
+        </Routes>
+       </div>
+  )
+}
+
+export default App
